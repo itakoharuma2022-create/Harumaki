73e4e91aca5af65b69fb0d82f4765c28e2398c53
Harumaki <itakoharuma2022@gmail.com>
2026-09-09 21:08:32 +0900

    AppDb.kt の削除

--- app/src/main/java/com/harumaki/app/AppDb.kt
+++
@@ -1,4 +0,0 @@
-package com.harumaki.app
-import androidx.room.*
-@Database(entities=[TrailPoint::class],version=1,exportSchema=false)
-abstract class AppDb:RoomDatabase(){abstract fun trailDao():TrailDao}
